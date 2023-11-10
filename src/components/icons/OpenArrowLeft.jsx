19307ac1eb0e41ba3f7bd7cb12e9bd6c19edd2aa
Alberto Caparrós Trigo <Alberto.Caparros.Trigo@unit4.com>
2023-11-10 17:36:57 +0100

    Add Icon arrow left

---
+++ src/components/icons/OpenArrowLeft.jsx
@@ -0,0 +1,15 @@
+function OpenArrowleft (props) {
+  return (
+    <svg
+      viewBox='0 0 465 1000'
+      fill='currentColor'
+      height='1.4em'
+      width='1em'
+      {...props}
+    >
+      <path d='M452 870c17.333 17.333 17.333 33.333 0 48-17.333 17.333-33.333 17.333-48 0L12 524c-16-16-16-32.667 0-50L404 80c14.667-17.333 30.667-17.333 48 0 17.333 14.667 17.333 30.667 0 48L94 500l358 370' />
+    </svg>
+  )
+}
+
+export default OpenArrowleft
